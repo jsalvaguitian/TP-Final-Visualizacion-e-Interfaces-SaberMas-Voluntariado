02ade498823038e97787c646ec1352dc59a902f8
jsalvaguitian <jsalvaguitian@frba.utn.edu.ar>
2025-07-17 03:32:48 -0300

    home responsive

--- script/home.js
+++ script/home.js
@@ -9,3 +9,36 @@ const leftBtn = document.getElementById('leftBtn');
   rightBtn.addEventListener('click', () => {
     track.scrollBy({ left: 200, behavior: 'smooth' });
   });
+
+  const carrouselContainer = document.getElementById('carrousel-cards');
+const flechaIzq = document.getElementById('flechaIzq');
+const flechaDer = document.getElementById('flechaDer');
+
+
+flechaDer.addEventListener("click", () => {
+  const firstCard = carrouselContainer.querySelector('.card');
+  
+  // Animación opcional (deslizamiento)
+  carrouselContainer.style.transition = 'transform 0.3s ease-in-out';
+  carrouselContainer.style.transform = 'translateX(-320px)';
+
+  setTimeout(() => {
+    carrouselContainer.style.transition = 'none';
+    carrouselContainer.style.transform = 'translateX(0)';
+    carrouselContainer.appendChild(firstCard);
+  }, 300);
+});
+
+flechaIzq.addEventListener("click", () => {
+  const cards = carrouselContainer.querySelectorAll('.card');
+  const lastCard = cards[cards.length - 1];
+
+  carrouselContainer.style.transition = 'none';
+  carrouselContainer.prepend(lastCard);
+  carrouselContainer.style.transform = 'translateX(-320px)';
+
+  setTimeout(() => {
+    carrouselContainer.style.transition = 'transform 0.3s ease-in-out';
+    carrouselContainer.style.transform = 'translateX(0)';
+  }, 10);
+});
